5502da1e1be8ab1e008335fef5008247f9feb605
Ismail Kuplu <i.kuplu@outlook.com>
2020-09-23 11:17:41 +0200

    Added query selectors for input values

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("hello");
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+console.log(celciusInput);
